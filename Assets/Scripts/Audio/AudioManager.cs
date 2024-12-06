376042d068e11795af3deb79884ffd13424019e3
Diem1070 <diemhuynh1070@gmail.com>
2024-12-06 14:29:37 +0700

    Update AudioManager.cs

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -3,58 +3,48 @@ using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager instance;
+    private static AudioManager _instance;
 
-    public AudioSource bgmSource;
-    public AudioSource sfxSource;
-    public Slider bgmSlider;
-    public Slider sfxSlider;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<AudioManager>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject("AudioManager");
+                    _instance = obj.AddComponent<AudioManager>();
+                }
+            }
+            return _instance;
+        }
+    }
 
     private void Awake()
     {
-        if (instance == null)
+        if (_instance == null)
         {
-            instance = this;
+            _instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        else
+        else if (_instance != this)
         {
             Destroy(gameObject);
         }
     }
 
-    private void Start()
-    {
-        // Gán giá tr? ban ??u cho slider
-        bgmSlider.value = bgmSource.volume;
-        sfxSlider.value = sfxSource.volume;
 
-        // L?ng nghe s? thay ??i giá tr? c?a slider
-        bgmSlider.onValueChanged.AddListener(SetBGMVolume);
-        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
-    }
 
-    // B?t ho?c t?t BGM
-    public void ToggleBGM(bool isOn)
-    {
-        bgmSource.mute = !isOn;
-    }
+    [SerializeField] AudioSource bgmSource;
+    [SerializeField] AudioSource sfxSource;
 
-    // B?t ho?c t?t SFX
-    public void ToggleSFX(bool isOn)
-    {
-        sfxSource.mute = !isOn;
-    }
-
-    // Ch?nh âm l??ng BGM
-    public void SetBGMVolume(float volume)
-    {
-        bgmSource.volume = volume;
-    }
-
-    // Ch?nh âm l??ng SFX
-    public void SetSFXVolume(float volume)
-    {
-        sfxSource.volume = volume;
-    }
+    public AudioClip bgm;
+    public AudioClip select;
+    public AudioClip deselect;
+    public AudioClip ;
+       ;
+    [SerializeField] Slider bgmSlider;
+    [SerializeField] Slider sfxSlider;
 }
