9c73460047f6098cac5c251ced37107cc37fede7
Diem1070 <diemhuynh1070@gmail.com>
2024-12-06 14:37:55 +0700

    Update AudioManager.cs

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager _instance;
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<AudioManager>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject("AudioManager");
+                    _instance = obj.AddComponent<AudioManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+    [SerializeField] AudioSource bgmSource;
+    [SerializeField] AudioSource sfxSource;
+
+    public AudioClip bgm;
+    public AudioClip select;
+    public AudioClip deselect;
+  
+    [SerializeField] Slider bgmSlider;
+    [SerializeField] Slider sfxSlider;
+}
